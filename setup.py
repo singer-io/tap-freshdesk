f4ac274150d5f9381030376e8543aa9f2b79d5cd
John Miller <jmiller@rjmetrics.com>
2017-03-09 15:55:02 -0500

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-freshdesk',
-      version='0.6.4',
+      version='0.6.5',
       description='Singer.io tap for extracting data from the Freshdesk API',
       author='Stitch',
       url='http://singer.io',
