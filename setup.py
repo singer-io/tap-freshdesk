cd3df2f447ad90f794bee7772069e01b746755fc
Chris Capurso <chrisc@stitchdata.com>
2018-02-14 15:25:46 -0500

    bump version to 0.8.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-freshdesk',
-      version='0.8.0',
+      version='0.8.1',
       description='Singer.io tap for extracting data from the Freshdesk API',
       author='Stitch',
       url='http://singer.io',
