79e63633471ea809db23c16dccc9017e3eb2e012
Eduard Vlasov <e.vlasov@realtimeboard.com>
2018-11-05 11:46:28 +0500

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-freshdesk',
-      version='0.9.1',
+      version='0.10.0',
       description='Singer.io tap for extracting data from the Freshdesk API',
       author='Stitch',
       url='http://singer.io',
