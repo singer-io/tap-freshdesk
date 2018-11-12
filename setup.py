38bbf068c7a9ccece3192eddffc8fcd8b7c92453
Nick McCoy <nick@stitchdata.com>
2018-11-12 13:43:29 +0000

    bump version of requests to 2.20.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(name='tap-freshdesk',
       py_modules=['tap_freshdesk'],
       install_requires=[
           'singer-python==5.2.3',
-          'requests==2.12.4',
+          'requests==2.20.0',
           'backoff==1.3.2'
       ],
       entry_points='''
