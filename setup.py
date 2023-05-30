d2f1179c5e5705e995b29f536e174e4d8bddf778
Andy Lu <andy@stitchdata.com>
2023-05-30 20:26:25 +0000

    Bump version, update changelog

--- setup.py
+++ setup.py
@@ -1,1 +1,36 @@
-clojure.lang.LazySeq@b1b77f26
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='tap-freshdesk',
+      version='0.11.1',
+      description='Singer.io tap for extracting data from the Freshdesk API',
+      author='Stitch',
+      url='http://singer.io',
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      py_modules=['tap_freshdesk'],
+      install_requires=[
+          'singer-python==5.2.3',
+          'requests==2.31.0',
+          'backoff==1.3.2'
+      ],
+      entry_points='''
+          [console_scripts]
+          tap-freshdesk=tap_freshdesk:main
+      ''',
+      packages=['tap_freshdesk'],
+      package_data = {
+          'tap_freshdesk/schemas': [
+              'agents.json',
+              'companies.json',
+              'contacts.json',
+              'conversations.json',
+              'groups.json',
+              'roles.json',
+              'satisfaction_ratings.json',
+              'tickets.json',
+              'time_entries.json',
+          ],
+      },
+      include_package_data=True,
+)
