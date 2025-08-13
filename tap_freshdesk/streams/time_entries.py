0f3d913685d33ee0b73eafb3b9f592d495871ce9
“rdeshmukh15” <rutuja.deshmukh@qlik.com>
2025-08-14 00:37:22 +0530

    child stream changes

--- tap_freshdesk/streams/time_entries.py
+++ tap_freshdesk/streams/time_entries.py
@@ -2,22 +2,20 @@ from typing import Any, Dict
 
 from singer import get_logger
 
-from tap_freshdesk.streams.abstracts import IncrementalStream
+from tap_freshdesk.streams.abstracts import ChildBaseStream
 
 LOGGER = get_logger()
 
 
-class TimeEntries(IncrementalStream):
+class TimeEntries(ChildBaseStream):
     tap_stream_id = "time_entries"
     key_properties = ["id"]
     replication_keys = ["updated_at"]
     path = "tickets/{}/time_entries"
     parent = "tickets"
 
-    def get_url_endpoint(self, parent_obj=None):
-        return f"{self.client.base_url}/{self.path.format(parent_obj['id'])}"
-
-    def write_bookmark(self, state: dict, key: Any = None, value: Any = None) -> Dict:
-        """A wrapper for singer.get_bookmark to deal with compatibility for
-        bookmark values or start values."""
-        pass
+    def modify_object(self, record: Dict, parent_record: Dict = None) -> Dict:
+        """Modify the record before writing to the stream."""
+        record["parent_id"] = parent_record["id"]
+        record = super().modify_object(record, parent_record)
+        return record
